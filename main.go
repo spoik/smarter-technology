6d4b2b28ae0f5ffc268e8607368023bdf46767eb
spike <sebfrye@gmail.com>
2026-03-10 16:19:11 -0500

    Created simple hello world server

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
     fmt.Println("Hello World?")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "Hello World!")
+    })
+
+    err := http.ListenAndServe(":80", nil)
+
+    if err != nil {
+        fmt.Printf("Failed to start server: %s\n", err)
+    }
 }
